e79251804f29d1c23e498f29413fed5451abe3ab
Noah Reinhardt <ndrein@gmail.com>
2016-09-26 11:53:37 -0400

    Tweak command line options

--- utils/log_report/log_report.py
+++ utils/log_report/log_report.py
@@ -26,19 +26,19 @@ def parse_args():
     parser.add_argument('job_file', help='Path to file containing jobs and associated info')
 
     parser.add_argument('-r', '--report', help='Display a text report summarizing the jobs and their status', action='store_true')
-    parser.add_argument('-dy', '--dependency_first', help="Display an indented text showing job dependencies from first completed to last completed", action='store_true')
-    parser.add_argument('-dr', '--depender_first', help="Display an indented text showing job dependencies from last completed to first completed", action='store_true')
+    parser.add_argument('-dy', '--dependency-first', help="Display an indented text showing job dependencies from first completed to last completed", action='store_true')
+    parser.add_argument('-dr', '--depender-first', help="Display an indented text showing job dependencies from last completed to first completed", action='store_true')
 
     # We can't allow 'success', 'nosucess', and 'minimal_detail' together, since minimal_detail is required for status to be defined
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-s', '--success', help='Show successful jobs only', action='store_true')
     group.add_argument('-nos', '--nosuccess', help='Show unsuccessful jobs only i.e. failed or uncompleted jobs', action='store_true')
-    group.add_argument('-m', '--minimal_detail', help="Only aggregate minimal information about each job", action='store_true')
+    group.add_argument('-m', '--minimal-detail', help="Only aggregate minimal information about each job", action='store_true')
 
     args = parser.parse_args()
 
     if not (args.report or args.dependency_first or args.depender_first):
-        parser.error('One of --report, --dependency_first, or --depender_first is required')
+        parser.error('One of --report, --dependency-first, or --depender-first is required')
 
     return args
 

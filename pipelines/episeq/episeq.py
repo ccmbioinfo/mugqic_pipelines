8a98ec9391db4f90a528edf53414fd0d8c278373
Michael Li <michael.li@uwaterloo.ca>
2016-10-26 10:21:35 -0400

    Fixed output filename in trim galore. Could this be causing trouble? Also made a lot of command script formatting.

--- pipelines/episeq/episeq.py
+++ pipelines/episeq/episeq.py
@@ -36,66 +36,6 @@ class Episeq(common.Illumina):
         self.argparser.add_argument("-d", "--design", help="design file", type=file)
         super(Episeq, self).__init__()
 
-    def merge_fastq(self):
-
-        """
-        This step merges FASTQ files for samples with multiple readsets or creates symbolic links of FASTQ
-        files for samples with only a single readset
-        """
-
-        jobs = []
-        for sample in self.samples:
-            merge_directory = os.path.join("merged", sample.name)
-            merge_prefix = os.path.join(merge_directory, sample.name)
-            mkdir_job = Job(command="mkdir -p " + merge_directory)
-            # If one readset in a sample is of a particular type,
-            # then all readsets are assumed to be of that type as well
-            run_type = sample.readsets[0].run_type
-
-            job = []
-            # Samples with only one readset do not require to be merged.
-            # A symbolic link to the FASTQ files are made instead
-            if len(sample.readsets) == 1:
-                target_fastq1 = sample.readsets[0].fastq1
-                if run_type == "PAIRED_END":
-                    target_fastq2 = sample.readsets[0].fastq2
-                elif run_type == "SINGLE_END":
-                    target_fastq2 = ""
-
-                job = concat_jobs([
-                    mkdir_job,
-                    Job([target_fastq1], [merge_prefix + "_R1.fastq.gz"],
-                        command="ln -s -f " + target_fastq1 + " " + merge_prefix + "_R1.fastq.gz"),
-                    Job([target_fastq2], [merge_prefix + "_R2.fastq.gz" if run_type == "PAIRED_END" else ""],
-                        command="ln -s -f " + target_fastq2 + " " + merge_prefix + "_R2.fastq.gz")
-                ], name="symlink_fastq." + sample.name)
-
-            # Samples with more than one readsets will be merged by their forward and/or reverse FASTQ files
-            elif len(sample.readsets) > 1:
-                fastq1 = [readset.fastq1 for readset in sample.readsets]
-                if run_type == "PAIRED_END":
-                    fastq2 = [readset.fastq2 for readset in sample.readsets]
-                elif run_type == "SINGLE_END":
-                    fastq2 = ""
-
-                job = concat_jobs([
-                    mkdir_job,
-                    Job([fastq for fastq in fastq1],
-                        [merge_prefix + "_R1.fastq.gz"],
-                        command="cat {fastq1} > {merged_fastq1}".format(
-                            fastq1=" ".join(fastq1),
-                            merged_fastq1=merge_prefix + "_R1.fastq.gz")),
-                    Job([fastq for fastq in fastq2],
-                        [merge_prefix + "_R2.fastq.gz" if run_type == "PAIRED_END" else ""],
-                        command="cat {fastq2} > {merged_fastq2}".format(
-                            fastq2=" ".join(fastq2),
-                            merged_fastq2=merge_prefix + "_R2.fastq.gz"))
-                ], name="merge_fastq." + sample.name)
-
-            jobs.append(job)
-
-        return jobs
-
     def bismark_prepare_genome(self):
         """
         Bismark requires a processed reference genome to compare with the epigenome.
@@ -139,6 +79,9 @@ class Episeq(common.Illumina):
         jobs = []
         for sample in self.samples:
             for readset in sample.readsets:
+                if readset.bam:
+                    continue  # Skip this iteration
+                # Get metadata and names
                 run_type = readset.run_type
                 protocol = readset.library
                 trim_directory = os.path.join("trimmed", sample.name, readset.name)
@@ -150,7 +93,7 @@ class Episeq(common.Illumina):
                 # Below are the default output names when running in paired or single mode
                 if run_type == "PAIRED_END":
                     input_files = [readset.fastq1, readset.fastq2]
-                    output_files = [fq1_out + "_1_val_1.fq.gz", fq2_out + "_1_val_2.fq.gz"]
+                    output_files = [fq1_out + "_1_val_1.fq.gz", fq2_out + "_2_val_2.fq.gz"]
                 elif run_type == "SINGLE_END":
                     input_files = [readset.fastq1]
                     output_files = [fq1_out + "_trimmed.fq.gz"]
@@ -165,17 +108,16 @@ class Episeq(common.Illumina):
                         command="""\
     module load trim_galore/0.4.1
     module load cutadapt/1.10
-    trim_galore {protocol} {library_type} {non_directional} {other} --output_dir {directory} \
-    --fastqc_args "-t 4" {fastq1} {fastq2}
-        """.format(
+    trim_galore {protocol} {library_type} {non_directional} {other} \\
+    --output_dir {directory} --fastqc_args "-t 4" {fastq}
+            """.format(
                             library_type="--paired" if run_type == "PAIRED_END" else "",
                             protocol='--rrbs' if protocol == 'RRBS' else '',
                             non_directional='--non_directional' if run_type == 'PAIRED_END' and protocol == 'RRBS'
                             else '',
                             other=config.param("trim_galore", "other_options"),
                             directory=trim_directory,
-                            fastq1=input_files[0],
-                            fastq2='' if run_type == "SINGLE_END" else input_files[1]
+                            fastq=' '.join(input_files)
                         )
                     )], name="trim_galore." + readset.name)
                 jobs.append(job)
@@ -219,7 +161,7 @@ class Episeq(common.Illumina):
                             directory=align_directory,
                             other=config.param("bismark_align", "other_options"),
                             input="-1 {fastq1} -2 {fastq2}".format(fastq1=input_files[0], fastq2=input_files[1]) if
-                                run_type == "PAIRED_END" else "--single_end {fastq1}".format(fastq1=input_files[0]),
+                            run_type == "PAIRED_END" else "--single_end {fastq1}".format(fastq1=input_files[0]),
                             directional='--non_directional' if protocol == 'RRBS' else '',
                             basename=readset.name + '_aligned'
                         )
@@ -359,38 +301,39 @@ class Episeq(common.Illumina):
                     ["differential_methylated_pos", "module_mugqic_R_packages"]
                 ],
                 command="""\
-        mkdir -p {directory}
-        R --no-save --no-restore <<-EOF
-        suppressPackageStartupMessages(library(BiSeq))
-        suppressPackageStartupMessages(library(minfi))
-        rrbs <- readBismark(c{samples}, colData=DataFrame(group=factor(c{group}), row.names=c{sample_names}))
-        rrbs.filtered <- rrbs[apply(totalReads(rrbs), 1, function(x) any(x > {coverage})),]
-        beta <- methLevel(rawToRel(rrbs.filtered))
+mkdir -p {directory}
+R --no-save --no-restore <<-EOF
+suppressPackageStartupMessages(library(BiSeq))
+suppressPackageStartupMessages(library(minfi))
+rrbs <- readBismark(c{samples}, colData=DataFrame(group=factor(c{group}), row.names=c{sample_names}))
+rrbs.filtered <- rrbs[apply(totalReads(rrbs), 1, function(x) any(x > {coverage})),]
+beta <- methLevel(rawToRel(rrbs.filtered))
 
-        #Use M values to do statistical tests because they are more reliable
-        #dmpFinder does not work with M values that are 0 or INF so the beta values must be shifted slightly
-        #Although there is no such thing as a beta value > 1, it will not matter in this step because only
-        #the average beta values are shown to the user
-        beta[beta == 0] = 0.0001
-        beta[beta == 1] = 1.0001
-        M <- log2(beta/(1-beta))
+#Use M values to do statistical tests because they are more reliable
+#dmpFinder does not work with M values that are 0 or INF so the beta values must be shifted slightly
+#Although there is no such thing as a beta value > 1, it will not matter in this step because only
+#the average beta values are shown to the user
+beta[beta == 0] = 0.0001
+beta[beta == 1] = 1.0001
+M <- log2(beta/(1-beta))
 
-        dmp <- dmpFinder(M, pheno=colData(rrbs.filtered)[,"group"], type="categorical")
-        dmp["pval"] <- p.adjust(dmp[,"pval"], method = "{padjust_method}")
-        dmp <- dmp[dmp["pval"] < {pvalue},]["pval"]
+dmp <- dmpFinder(M, pheno=colData(rrbs.filtered)[,"group"], type="categorical")
+dmp["pval"] <- p.adjust(dmp[,"pval"], method = "{padjust_method}")
+dmp <- dmp[dmp["pval"] < {pvalue},]["pval"]
 
-        controls <- c{controls}
-        cases <- c{cases}
-        result = as.data.frame(rowRanges(rrbs.filtered))[1:4]
-        result["Avg Control Beta"] = rowMeans(beta[,controls])
-        result["Avg Case Beta"] = rowMeans(beta[,cases])
-        result["Avg Delta Beta"] = result[,"Avg Case Beta"] - result[,"Avg Control Beta"]
-        result <- merge(result, dmp, by=0)
-        result <- result[abs(result["Avg Delta Beta"]) > {delta_beta_threshold}]
+controls <- c{controls}
+cases <- c{cases}
+result = as.data.frame(rowRanges(rrbs.filtered))[1:4]
+result["Avg Control Beta"] = rowMeans(beta[,controls])
+result["Avg Case Beta"] = rowMeans(beta[,cases])
+result["Avg Delta Beta"] = result[,"Avg Case Beta"] - result[,"Avg Control Beta"]
+result <- merge(result, dmp, by=0)
+result <- result[abs(result["Avg Delta Beta"]) > {delta_beta_threshold}]
 
-        write.csv(result, file="{dmps_file}", quote=FALSE, row.names=FALSE)
+write.csv(result, file="{dmps_file}", quote=FALSE, row.names=FALSE)
 
-        EOF""".format(
+EOF
+                """.format(
                     directory=os.path.dirname(dmps_file),
                     samples=tuple(cov_files),
                     group=tuple(sample_group),
@@ -422,7 +365,7 @@ class Episeq(common.Illumina):
         for contrast in self.contrasts:
             # Determine the control and case samples to include in the analysis from the contrast
             contrast_samples = [sample for sample in contrast.controls + contrast.treatments]
-            cov_files = [os.path.join("methyl_calls", sample.name + ".merged.bismark.cov.gz") for sample in
+            cov_files = [os.path.join("methyl_calls", sample.name, sample.name + ".merged.bismark.cov.gz") for sample in
                          contrast_samples]
             sample_group = ["control" if sample in contrast.controls else "case" for sample in contrast_samples]
             dmrs_file = os.path.join("differential_methylated_regions",
@@ -436,39 +379,40 @@ class Episeq(common.Illumina):
                     ["differential_methylated_regions", "module_mugqic_R_packages"]
                 ],
                 command="""\
-        mkdir -p {directory}
-        R --no-save --no-restore <<-EOF
-        suppressPackageStartupMessages(library(bumphunter))
-        suppressPackageStartupMessages(library(BiSeq))
-        library(doParallel)
-        registerDoParallel(cores={cores})
+mkdir -p {directory}
+R --no-save --no-restore <<-EOF
+suppressPackageStartupMessages(library(bumphunter))
+suppressPackageStartupMessages(library(BiSeq))
+library(doParallel)
+registerDoParallel(cores={cores})
 
-        rrbs <- readBismark(c{samples}, colData=DataFrame(group=c{group}, row.names=c{sample_names}))
-        rrbs.filtered <- rrbs[apply(totalReads(rrbs), 1, function(x) any(x > {coverage})),]
-        beta <- methLevel(rawToRel(rrbs.filtered))
-        chr <- as.character(seqnames(rowRanges(rrbs.filtered)))
-        pos <- start(ranges(rowRanges(rrbs.filtered)))
-        pheno <- colData(rrbs.filtered)[,"group"]
-        designM <- model.matrix(~pheno)
+rrbs <- readBismark(c{samples}, colData=DataFrame(group=c{group}, row.names=c{sample_names}))
+rrbs.filtered <- rrbs[apply(totalReads(rrbs), 1, function(x) any(x > {coverage})),]
+beta <- methLevel(rawToRel(rrbs.filtered))
+chr <- as.character(seqnames(rowRanges(rrbs.filtered)))
+pos <- start(ranges(rowRanges(rrbs.filtered)))
+pheno <- colData(rrbs.filtered)[,"group"]
+designM <- model.matrix(~pheno)
 
-        dmrs <- bumphunterEngine(beta,
-                                 chr=chr,
-                                 pos=pos,
-                                 design=designM,
-                                 cutoff={delta_beta_threshold},
-                                 pickCutoffQ=0.99,
-                                 null_method=c("permutation","bootstrap"),
-                                 smooth=FALSE,
-                                 smoothFunction=locfitByCluster,
-                                 B={permutations},
-                                 verbose=TRUE,
-                                 maxGap=500)
+dmrs <- bumphunterEngine(beta,
+                         chr=chr,
+                         pos=pos,
+                         design=designM,
+                         cutoff={delta_beta_threshold},
+                         pickCutoffQ=0.99,
+                         null_method=c("permutation","bootstrap"),
+                         smooth=FALSE,
+                         smoothFunction=locfitByCluster,
+                         B={permutations},
+                         verbose=TRUE,
+                         maxGap=500)
 
-        dmrs <- na.omit(dmrs)
+dmrs <- na.omit(dmrs)
 
-        write.csv(dmrs\$table, "{dmrs_file}", quote=FALSE, row.names=FALSE)
+write.csv(dmrs\$table, "{dmrs_file}", quote=FALSE, row.names=FALSE)
 
-        EOF""".format(
+EOF
+                """.format(
                     directory=os.path.dirname(dmrs_file),
                     samples=tuple(cov_files),
                     group=tuple(sample_group),

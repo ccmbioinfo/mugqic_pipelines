e5cd53ff0b363e5051acc45be6d742346b71d795
Michael Li <michael.li@uwaterloo.ca>
2016-11-03 13:55:45 -0400

    All the small things

--- pipelines/episeq/episeq.py
+++ pipelines/episeq/episeq.py
@@ -211,7 +211,7 @@ class Episeq(common.Illumina):
                 job = concat_jobs([
                     mkdir_job,
                     Job(
-                        input_files + ["Bisulfite_Genome"],
+                        input_files + ["bismark_prepare_genome/Bisulfite_Genome"],
                         [readset_sam] + report_log,
                         [["bismark_align", "module_bowtie2"],
                          ["bismark_align", "module_samtools"],
@@ -494,7 +494,7 @@ bismark_methylation_extractor {library_type} {other} --multicore {core} --output
         for contrast in self.contrasts:
             # Determine the control and case samples to include in the analysis from the contrast
             contrast_samples = [sample for sample in contrast.controls + contrast.treatments]
-            cov_files = [os.path.join("methyl_calls",sample.name , sample.name + ".merged.bismark.cov.gz") for
+            cov_files = [os.path.join("methyl_calls", sample.name ,sample.name + ".merged.bismark.cov.gz") for
                          sample in contrast_samples]
             sample_group = ["control" if sample in contrast.controls else "case" for sample in contrast_samples]
             dmps_file = os.path.join("differential_methylated_positions",

b33531bf4ceac834661e249f574f702764e39cd9
Noah Reinhardt <ndrein@gmail.com>
2016-11-22 10:33:43 -0500

    Add trinity

--- pipelines/metatranscriptomics/execution-scripts/add_duplicates.sh
+++ pipelines/metatranscriptomics/execution-scripts/add_duplicates.sh
@@ -2,4 +2,6 @@
 
 mkdir add_duplicates
 
+module load perl
+
 perl /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/main_remove_derepli.pl
---
+++ pipelines/metatranscriptomics/execution-scripts/trinity_assembly.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+#PBS -N Job_trinity
+#PBS -l nodes=1:ppn=8
+#PBS -l gres=localhd:20
+#PBS -l vmem=40g
+#PBS -l walltime=48:00:00
+#PBS -joe /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/execution-scripts
+
+
+module load trinityrnaseq/2.1.1
+module load bowtie/1.0.1
+module load samtools/1.2
+
+my_WORKDIR=/hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/execution-scripts
+cd ${my_WORKDIR}
+
+indir=add_duplicates
+outdir=trinity_assembly
+
+rm -fr trinity_assembly
+mkdir trinity_assembly
+
+Trinity --seqType fq --left $indir/cow1_mRNA.fastq --right $indir/cow2_mRNA.fastq --CPU 8 --max_memory 10G --min_contig_length 75 --output ${my_WORKDIR}/${outdir} --full_cleanup --no_version_check
+
+cp ${my_WORKDIR}/${outdir}/Trinity.fasta ${my_WORKDIR}/$outdir/cow_contigs.fasta

fe6b40ea740554f57da1333b45774c9677441833
Noah Reinhardt <ndrein@gmail.com>
2016-11-23 17:28:31 -0500

    Add searching microbial database with bwa

---
+++ pipelines/metatranscriptomics/execution-scripts/bwa_search_with_singletons.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+module load mugqic-pipelines/2.2.0
+module load perl
+module load mugqic/bwa/0.7.12
+module load mugqic/samtools/1.3
+module load blastp/2.2.27
+
+input_dir=map_reads
+contigs_dir=index_contigs
+singletons_dir=map_reads
+output_dir=bwa_search_with_singletons
+reference_dir=../reference-files
+
+mkdir $output_dir
+
+
+bwa aln -t 4 $reference_dir/microbial_all_cds.fasta $contigs_dir/cow1_singletons.fastq > $output_dir/cow1_singletons.sai
+bwa aln -t 4 $reference_dir/microbial_all_cds.fasta $contigs_dir/cow2_singletons.fastq > $output_dir/cow2_singletons.sai
+bwa sampe $reference_dir/microbial_all_cds.fasta $output_dir/cow1_singletons.sai  $output_dir/cow2_singletons.sai $output_dir/cow1_singletons.fastq $output_dir/cow2_singletons.fastq > $output_dir/cow_singletons.sam
+samtools view -bS $output_dir/cow_singletons.sam | samtools sort -n -o $output_dir/cow_singletons.bam
+samtools view -F 4 $output_dir/cow_singletons.bam > $output_dir/cow_singletons_micro_cds.bwaout
+
+perl /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/main_read_samout.pl \
+    $output_dir/cow_singletons_micro_cds.bwaout \
+    $output_dir/cow_singletons_micro_cds_bwa_IDs.txt \
+    $output_dir/cow_singletons_micro_cds_bwa_pairs.txt \
+    $output_dir/cow_singletons_micro_cds_bwa_hitsID.txt
+#perl main_read_samout.pl cow microgenes bwa singletons micro_cds
+
+
+python /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/split_reads_by_id.py \
+    --fastq $singletons_dir/cow1_singletons.fastq \
+    --id-file $output_dir/cow_singletons_micro_cds_bwa_IDs.txt \
+    --included $output_dir/cow1_singletons_micro_cds.fasta \
+    --excluded $output_dir/cow1_singletons_n_micro_cds.fasta
+python /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/split_reads_by_id.py \
+    --fastq $singletons_dir/cow2_singletons.fastq \
+    --id-file $output_dir/cow_singletons_micro_cds_bwa_IDs.txt \
+    --included $output_dir/cow2_singletons_micro_cds.fasta \
+    --excluded $output_dir/cow2_singletons_n_micro_cds.fasta
+#perl main_select_reads_fromfile.pl cow microgenes bwa singletons micro_cds
+

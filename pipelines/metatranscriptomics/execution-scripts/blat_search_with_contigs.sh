7a17df09b96ca67a2a39c8e6dafc4775703f9faa
Noah Reinhardt <ndrein@gmail.com>
2016-11-24 13:53:10 -0500

    Fix bugs

--- pipelines/metatranscriptomics/execution-scripts/blat_search_with_contigs.sh
+++ pipelines/metatranscriptomics/execution-scripts/blat_search_with_contigs.sh
@@ -10,6 +10,7 @@ module load blat/35
 module load mugqic-pipelines/2.2.0
 
 blastdb=../reference-files
+map_reads=map_reads
 input_dir=bwa_search_with_contigs
 output_dir=blat_search_with_contigs
 
@@ -26,12 +27,14 @@ cat $output_dir/cow_contigs_1.blatout $output_dir/cow_contigs_2.blatout > $outpu
 # main_sort_blastout_fromfile.pl
 perl /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/main_sort_blastout_fromfile.pl \
     $output_dir/cow_contigs_n_micro_cds.blatout \
-    $output_dir/cow_conigs_n_micro_cds_sorted.blatout
+    $output_dir/cow_contigs_n_micro_cds_sorted.blatout \
+    10
 #perl main_sort_blastout_fromfile.pl cow n_micro_cds blat contigs 10
 
 # main_get_blast_fromfile_1tophit.pl
 perl /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/main_get_blast_fromfile_1tophit.pl \
     $output_dir/cow_contigs_n_micro_cds_sorted.blatout \
+    $map_reads/cow_contigs_IDs_length.txt \
     $output_dir/cow_contigs_n_micro_cds_blat_IDs.txt \
     $output_dir/cow_contigs_n_micro_cds_blat_pairs.txt \
     $output_dir/cow_contigs_n_micro_cds_blat_hitsID.txt \
@@ -40,7 +43,7 @@ perl /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscr
 
 # split_reads_by_id.py
 python /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/split_reads_by_id.py \
-    --fastq $input_dir/cow_contigs_n_micro_cds.fasta \
+    --fasta $input_dir/cow_contigs_n_micro_cds.fasta \
     --id-file $output_dir/cow_contigs_n_micro_cds_blat_IDs.txt \
     --included $output_dir/cow_contigs_n_micro_cds_blat.fasta \
     --excluded $output_dir/cow_contigs_n_micro_cds_rest.fasta
--- pipelines/metatranscriptomics/scripts/split_reads_by_id.py
+++ pipelines/metatranscriptomics/scripts/split_reads_by_id.py
@@ -22,9 +22,13 @@ ids = {line.split('\t')[0] for line in open(args.id_file)}
 
 input, format = (args.fastq, 'fastq') if args.fastq else (args.fasta, 'fasta')
 all_reads = set(SeqIO.parse(input, format))
+print('Total number of reads: {}'.format(len(all_reads)))
 
 included_reads = {read for read in all_reads if read.id in ids}
 excluded_reads = all_reads - included_reads
+
+print('Number of included reads: {}'.format(len(included_reads)))
+print('Number of excluded reads: {}'.format(len(excluded_reads)))
 
 SeqIO.write(included_reads, args.included, format)
 SeqIO.write(excluded_reads, args.excluded, format)

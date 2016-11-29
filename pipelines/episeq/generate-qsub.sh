3de75e3e30da23c49f88189574f622ac78af548d
Michael Li <michael.li@uwaterloo.ca>
2016-11-29 13:42:32 -0500

    Cleaned up generating script and used relative paths as an example.

--- pipelines/episeq/generate-qsub.sh
+++ pipelines/episeq/generate-qsub.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-/hpf/projects/brudno/lmichael/mugqic_pipelines/pipelines/episeq/episeq.py \
- -s 1-7 \
- -o /hpf/projects/brudno/lmichael/episeq_run/output \
+# Setup environment to run MUGQIC pipelines
+module load mugqic-pipelines/2.2.0
+
+# Setup path variable for python. Modify these paths to point the
+export PYTHONPATH=/hpf/projects/brudno/lmichael/test2/:$PYTHONPATH
+
+./episeq/episeq.py \
+ -s 1-13 \
+ -o output \
  -j pbs \
  -l debug \
- -d /hpf/projects/brudno/lmichael/episeq_run/episeq.design \
- -r /hpf/projects/brudno/lmichael/episeq_run/episeq.readset \
- -c episeq.ini 1> qsub.sh 2> debug.log 
+ -d ./episeq.design \
+ -r ./episeq.readset \
+ -c episeq.ini 1> qsub.sh 2> debug.log
+
+# Other flags
+#--report || --clean
 
 chmod +x qsub.sh
 

002a1d547939febaed64b66cae1f7a6a253639b7
Noah Reinhardt <ndrein@gmail.com>
2016-11-30 15:50:22 -0500

    Refactor flash to take output dir and prefix

--- bfx/flash.py
+++ bfx/flash.py
@@ -6,13 +6,15 @@ from core.config import config
 from core.job import Job
 
 
-def flash(fastq1, fastq2):
+def merge_short_reads(fastq1, fastq2, output_dir, output_prefix):
     return Job(
         # TODO: specify input and output (when pipeline is more mature)
         # input_files=[fastq1, fastq2],
         # output_files=NotImplementedError,
         module_entries=['flash', 'module_flash'],
-        command='flash {options} ' \
-                '{fastq1} {fastq2}'.format(options=config.param('flash', 'options'),
+        command='flash -d {output_dir} -o {output_prefix} {options} ' \
+                '{fastq1} {fastq2}'.format(output_dir=output_dir,
+                                           output_prefix=output_prefix,
+                                           options=config.param('flash', 'options'),
                                            fastq1=fastq1,
                                            fastq2=fastq2))

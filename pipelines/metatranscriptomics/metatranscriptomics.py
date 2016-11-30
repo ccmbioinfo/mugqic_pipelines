830828bb746cb50325ab3d56a93d9b7a2048d79f
Noah Reinhardt <ndrein@gmail.com>
2016-11-30 15:50:43 -0500

    Redo flash step

--- pipelines/metatranscriptomics/metatranscriptomics.py
+++ pipelines/metatranscriptomics/metatranscriptomics.py
@@ -14,11 +14,12 @@ from core.config import config
 from core.job import Job, concat_jobs
 from pipelines import common
 from bfx import trimmomatic
-
+from bfx import flash
 
 log = logging.getLogger(__name__)
 
 
+# TODO: specify input/output files
 class Metatranscriptomics(common.Illumina):
     # Location for pipeline scripts, to be used by all steps
     # 'metatranscriptomics/scripts'
@@ -56,37 +57,43 @@ class Metatranscriptomics(common.Illumina):
         output_unpaired2 = join(output_dir, 'cow2_qual_unpaired.fastq')
 
         job = trimmomatic.trimmomatic(input1,
-                                        input2,
-                                        output_paired1,
-                                        output_unpaired1,
-                                        output_paired2,
-                                        output_unpaired2,
-                                        None,
-                                        None,
-                                        adapter_file=config.param('trimmomatic', 'adapter_fasta'),
-                                        trim_log=join(output_dir, 'cow.trim.log'))
+                                      input2,
+                                      output_paired1,
+                                      output_unpaired1,
+                                      output_paired2,
+                                      output_unpaired2,
+                                      None,
+                                      None,
+                                      adapter_file=config.param('trimmomatic', 'adapter_fasta'),
+                                      trim_log=join(output_dir, 'cow.trim.log'))
         job.name = 'trimmomatic.cow'
         return job
 
     def flash(self):
-        return [concat_jobs([Job(command='mkdir -p ' + 'flash'),
-                             Job(input_files=['trim/cow1_qual_paired.fastq',
-                                              'trim/cow2_qual_paired.fastq'],
-                                 output_files=['flash/cow_qual.extendedFrags.fastq',
-                                               'flash/cow_qual.hist',
-                                               'flash/cow_qual.histogram',
-                                               'flash/cow_qual.notCombined_1.fastq',
-                                               'flash/cow_qual.notCombined_2.fastq'],
-                                 command='{flash} -M 75 -p 64 -t 2 -o cow_qual -d flash '
-                                         'trim/cow1_qual_paired.fastq trim/cow2_qual_paired.fastq'.format(flash=config.param('flash', 'location'))),
-                             Job(input_files=['flash/cow_qual.extendedFrags.fastq',
-                                              'flash/cow_qual.notCombined_1.fastq'],
-                                 output_files=['flash/cow1_qual_all.fastq'],
-                                 command='cat flash/cow_qual.extendedFrags.fastq flash/cow_qual.notCombined_1.fastq > flash/cow1_qual_all.fastq'),
-                             Job(input_files=['flash/cow_qual.notCombined_2.fastq'],
-                                 output_files=['flash/cow2_qual_all.fastq'],
-                                 command='cp flash/cow_qual.notCombined_2.fastq flash/cow2_qual_all.fastq')],
-                            name='flash.cow')]
+        input = dict()
+        input['dir'] = 'format_reads'
+        input['fastq1'] = join(input['dir'], 'cow1_qual_paired.fastq')
+        input['fastq2'] = join(input['dir'], 'cow2_qual_paired.fastq')
+
+        output = dict()
+        output['dir'] = 'format_reads'
+        output['prefix'] = 'cow_qual'
+
+        output['notCombined1'] = join(output['dir'], output['prefix'] + '.notCombined_1.fastq')
+        output['notCombined2'] = join(output['dir'], output['prefix'] + '.notCombined_2.fastq')
+        output['merged'] = join(output['dir'], output['prefix'] + '.extendedFrags.fastq')
+
+        output[1] = join(output['dir'], 'cow1_qual_all.fastq')
+        output[2] = join(output['dir'], 'cow2_qual_all.fastq')
+
+        return [concat_jobs([flash.merge_short_reads(input['fastq1'], input['fastq2'], output['dir'], output['prefix']),
+                             Job(command='cat {merged} {notCombined1} > {output1}'.format(merged=output['merged'],
+                                                                                          notCombined1=output[
+                                                                                              'notCombined1'],
+                                                                                          output1=output[1])),
+                             Job(command='cp {notCombined2} {output2}'.format(notCombined2=output['notCombined2'],
+                                                                              output2=output[2]))],
+                            name='flash')]
 
     def fastq_to_fasta(self):
         return [concat_jobs([Job(command='mkdir -p remove_duplicates'),
@@ -136,7 +143,8 @@ usearch --derep_fullseq --cluster remove_duplicates/cow2_qual_all.fasta --seedso
                             '--tblout remove_abundant_rrna/cow1_rRNA.infernalout '
                             '--noali --notextw --rfam -E 0.001 '
                             '{rfam} '
-                            'remove_duplicates/cow1_qual_all_unique.fasta'.format(rfam=config.param('remove_abundant_rrna', 'rfam_location'))),
+                            'remove_duplicates/cow1_qual_all_unique.fasta'.format(
+                        rfam=config.param('remove_abundant_rrna', 'rfam_location'))),
                 Job(input_files=['remove_duplicates/cow2_qual_all_unique.fasta'],
                     output_files=['remove_abundant_rrna/cow2_rRNA.log',
                                   'remove_abundant_rrna/cow2_rRNa.infernalout'],
@@ -148,8 +156,9 @@ usearch --derep_fullseq --cluster remove_duplicates/cow2_qual_all.fasta --seedso
                             '--tblout remove_abundant_rrna/cow2_rRNA.infernalout '
                             '--noali --notextw --rfam -E 0.001 '
                             '{rfam} '
-                            'remove_duplicates/cow2_qual_all_unique.fasta'.format(rfam=config.param('remove_abundant_rrna', 'rfam_location')))
-                ], name='remove_abundant_rrna.cow'
+                            'remove_duplicates/cow2_qual_all_unique.fasta'.format(
+                        rfam=config.param('remove_abundant_rrna', 'rfam_location')))
+            ], name='remove_abundant_rrna.cow'
             )
         ]
 
@@ -160,9 +169,10 @@ usearch --derep_fullseq --cluster remove_duplicates/cow2_qual_all.fasta --seedso
             self.trimmomatic,
             self.flash,
             self.fastq_to_fasta,
-            self.remove_duplicates, #5
+            self.remove_duplicates,  # 5
             self.remove_abundant_rrna,
         ]
+
 
 if __name__ == '__main__':
     Metatranscriptomics()

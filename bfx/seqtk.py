c9bbb3cef3245e1871ebc3eacbd9458310465da4
Noah Reinhardt <ndrein@gmail.com>
2016-12-01 16:36:49 -0500

    Add fastq_to_fasta in bfx/seqtk.py

---
+++ bfx/seqtk.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env
+
+from core.job import Job
+
+
+def fastq_to_fasta(fastq, fasta):
+    return Job(
+        input_files=[fastq],
+        output_files=[fasta],
+        module_entries=[['seqtk', 'module_seqtk']],
+        command='seqtk seq -a {fastq} > {fasta}'.format(fastq=fastq,
+                                                        fasta=fasta)
+    )

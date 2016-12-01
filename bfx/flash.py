6206c95b2cc4b58ab8e84c8df49510b534a65871
Noah Reinhardt <ndrein@gmail.com>
2016-12-01 14:39:53 -0500

    IO files in flash

--- bfx/flash.py
+++ bfx/flash.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
 
+from os.path import join
 
 # MUGQIC Modules
 from core.config import config
 from core.job import Job
 
 
-def merge_short_reads(fastq1, fastq2, output_dir, output_prefix):
+def merge_overlapping_reads(fastq1, fastq2, output_dir, output_prefix):
     return Job(
-        # TODO: specify input and output (when pipeline is more mature)
-        # input_files=[fastq1, fastq2],
-        # output_files=NotImplementedError,
+        input_files=[fastq1, fastq2],
+        output_files=[join(output_dir, output_prefix + '.notCombined_1.fastq'),
+                      join(output_dir, output_prefix + '.notCombined_2.fastq'),
+                      join(output_dir, output_prefix + '.extendedFrags.fastq')],
         module_entries=['flash', 'module_flash'],
         command='flash -d {output_dir} -o {output_prefix} {options} ' \
                 '{fastq1} {fastq2}'.format(output_dir=output_dir,

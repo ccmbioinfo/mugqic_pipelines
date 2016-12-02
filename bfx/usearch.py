3a48d5392a69521b070dc12227c6d4deb94183d2
Noah Reinhardt <ndrein@gmail.com>
2016-12-02 15:51:08 -0500

    Add usearch.py

---
+++ bfx/usearch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from core.config import config
+from core.job import Job
+
+
+def cluster_duplicates(input_fasta, output_fasta, output_uc, name='cluster_duplicates'):
+    return Job(name=name,
+               input_files=[input_fasta],
+               output_files=[output_fasta, output_uc],
+               module_entries=[['DEFAULT', 'module_usearch']],
+               command='usearch --derep_fullseq --cluster {input_fasta} '
+                       '--seedsout {output_fasta} --sizeout '
+                       '--uc {output_uc}'.format(input_fasta=input_fasta,
+                                                 output_fasta=output_fasta,
+                                                 output_uc=output_uc))

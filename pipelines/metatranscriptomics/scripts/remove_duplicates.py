df246218b3420c16be50fd192fb2036bf81e7c10
Noah Reinhardt <ndrein@gmail.com>
2016-12-02 17:01:19 -0500

    Documentation

--- pipelines/metatranscriptomics/scripts/remove_duplicates.py
+++ pipelines/metatranscriptomics/scripts/remove_duplicates.py
@@ -29,16 +29,10 @@ def parse_args():
     return arg_parser.parse_args()
 
 
-def get_cluster_size(record):
-    # print(record.description)
-    return re.search('size=(\d+)', record.description).group(1)
-
-
 def get_unique_ids(uc_file):
     """
     Get the fastq IDs of reads that are unique
     """
-
     def get_id(line):
         return re.match('^(\S+\t){8}(\S+)', line).group(2)
 
@@ -52,7 +46,6 @@ def get_cluster_id_to_size(fasta):
     :param fasta: fasta that has been clustered with usearch
     :return: dict
     """
-
     def get_id(line):
         return re.search('Cluster(\d+);', line).group(1)
 
@@ -69,7 +62,6 @@ def get_fastq_id_to_cluster_id(uc_file):
     :param uc_file: *.uc from usearch
     :return: dict
     """
-
     def fastq_id(line):
         return re.match('^(\S+\t){8}(\S+)', line).group(2)
 
@@ -80,19 +72,40 @@ def get_fastq_id_to_cluster_id(uc_file):
 
 
 def get_unique_reads(fastq, uc_file):
+    """
+    Subset the reads from fastq to remove duplicates
+
+    :param fastq: original fastq with duplicates
+    :param uc_file: *.uc file
+    :return: list
+    """
     unique_ids = get_unique_ids(uc_file)
     return [read for read in SeqIO.parse(fastq, 'fastq') if read.id in unique_ids]
 
 
-def assign_cluster_size(reads, uc_file, unique_fasta):
+def assign_cluster_size(unique_reads, uc_file, unique_fasta):
+    """
+    Set the 'cluster_size' attribute on all unique reads
+
+    :param unique_reads: iterable of reads
+    :param uc_file: *.uc file
+    :param unique_fasta: output from usearch that contains the number of duplicates for each read
+    """
     fastq_id_to_cluster_id = get_fastq_id_to_cluster_id(uc_file)
     cluster_id_to_size = get_cluster_id_to_size(unique_fasta)
 
-    for read in reads:
+    for read in unique_reads:
         read.cluster_size = cluster_id_to_size[fastq_id_to_cluster_id[read.id]]
 
 
 def write_ids(reads, id_file):
+    """
+    Write to id_file:
+    <ID><TAB><number of duplicates>
+
+    :param reads: iterable of reads
+    :param id_file: *.IDs.txt
+    """
     # Write the ID file as <ID><TAB><number of duplicates>
     with open(id_file, 'w+') as f:
         for read in reads:
@@ -100,15 +113,27 @@ def write_ids(reads, id_file):
 
 
 def write_unique_reads(reads, file, format):
+    """
+    Write reads to file according to formatt
+
+    :param reads: iterable of reads
+    :param file: *.fastq file or *.fasta
+    :param format: 'fastq' or 'fasta'
+    :return:
+    """
     with open(file, 'w+') as out:
         SeqIO.write(reads, out, format)
 
 
 args = parse_args()
 
+# Get the unique reads and determine how many there are in each cluster
 unique_reads = get_unique_reads(args.original_fastq, args.unique_uc)
 assign_cluster_size(unique_reads, args.unique_uc, args.unique_fasta)
 
+# Keep track of how many duplicates there are of each read
 write_ids(unique_reads, args.output_ids)
+
+# Write out a *.fasta and *.fastq file containing only the unique reads
 write_unique_reads(unique_reads, args.unique_fastq, 'fastq')
 write_unique_reads(unique_reads, args.unique_fasta, 'fasta')

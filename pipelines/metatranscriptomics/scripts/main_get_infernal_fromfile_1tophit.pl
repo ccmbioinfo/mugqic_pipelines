4c46954c6cb61de26b6e5d8492f9e61a8d7ab046
Noah Reinhardt <ndrein@gmail.com>
2016-12-06 15:04:54 -0500

    Name variables properly for readability

--- pipelines/metatranscriptomics/scripts/main_get_infernal_fromfile_1tophit.pl
+++ pipelines/metatranscriptomics/scripts/main_get_infernal_fromfile_1tophit.pl
@@ -1,14 +1,14 @@
 #!/usr/bin/env perl
 use List::Util qw[min max];
 
-my ($read_files, $read_type, $cutoff_type, $cutoff1, $cutoff2) = @ARGV;
+my ($read_files, $read_type, $cutoff_type, $evalue_cutoff, $identity_cutoff) = @ARGV;
 
 my $Datapath = "~/CourseData/metagenomics/metatranscriptomics/";
 my $Workpath = "";
 
 if ($cutoff_type == 0) {
-    $cutoff1 = 0;
-    $cutoff2 = 0;
+    $evalue_cutoff = 0;
+    $identity_cutoff = 0;
 }
 
 my $II = 1;
@@ -19,30 +19,30 @@ if ($read_type eq 'pairs') {
 for (my $i = 1; $i <= $II; $i++) {
     my $lengthfile = "remove_rrna/cow" . $i . "_IDs_length.txt";
 #    my $lengthfile = "remove_rrna/cow" . $i . "_qual_all_unique_IDs_length.txt";
-    my $infile = "remove_rrna/cow" . $i . "_rRNA.infernalout";
+    my $infernalout = "remove_rrna/cow" . $i . "_rRNA.infernalout";
 
-    my $outfile1 = "remove_rrna/cow" . $i . "_rRNA_infernal_IDs.txt";
-    my $outfile2 = "remove_rrna/cow" . $i . "_rRNA_infernal_pairs.txt";
-    my $outfile3 = "remove_rrna/cow" . $i . "_rRNA_infernal_hitsID.txt";
-    print "Outputfile: $outfile1\n\n";
+    my $out_id_file = "remove_rrna/cow" . $i . "_rRNA_infernal_IDs.txt";
+    my $out_pairs_file = "remove_rrna/cow" . $i . "_rRNA_infernal_pairs.txt";
+    my $out_hits_id_file = "remove_rrna/cow" . $i . "_rRNA_infernal_hitsID.txt";
+    print "Outputfile: $out_id_file\n\n";
 
-    my %reads;
-    my %reads2;
+    my %id_to_length;
+    my %id_to_cluster_size;
     open(INPUT0, $lengthfile) or die "Error opening $lengthfile : $!\n";
     while( my $line = <INPUT0> ) {
         chomp($line);
         my @values = split(/\t/, $line);
-        $reads{$values[0]} = $values[2];
-        $reads2{$values[0]} = $values[1];
+        $id_to_length{$values[0]} = $values[2];
+        $id_to_cluster_size{$values[0]} = $values[1];
     }
     close INPUT0;
-    print "number of all unique reads:  ".keys( %reads ).".\n";
+    print "number of all unique reads:  ".keys( %id_to_length ).".\n";
 
-    my %IDs;
+    my %rna_id_to_cluster_size;
     my %pairs;
-    my %hits;
+    my %rrna_origin_to_cluster_size;
     my $max_evalue;
-    open(INPUT, $infile) or die "Error opening $infile : $!\n";
+    open(INPUT, $infernalout) or die "Error opening $infernalout : $!\n";
     while  (my $line0 = <INPUT>) {
         chomp($line0);
         if ($line0 !~ m/^\#/) {
@@ -76,39 +76,39 @@ for (my $i = 1; $i <= $II; $i++) {
 
             my @line = ($tmp1, $tmp2, $tmp3, $tmp4, $tmp5, $tmp6);
 
-            if (not exists $IDs{$line[1]}) {
+            if (not exists $rna_id_to_cluster_size{$line[1]}) {
                 $max_evalue = $line[5];
 
-                my $mydbID = $line[1];
+                my $ID = $line[1];
 
-                my $hitgiID = $line[0];
+                my $rrna_origin = $line[0];
 
-                my $pident;
-                if (not exists $reads{$mydbID}) {
-                    $reads{$mydbID} = 100;
-                    $reads2{$mydbID} = 2;
+                my $percent_identity;
+                if (not exists $id_to_length{$ID}) {
+                    $id_to_length{$ID} = 100;
+                   $id_to_cluster_size{$ID} = 2;
                 }
-                $pident = 100 * abs($line[3] - $line[2] + 1) / $reads{$mydbID};
-                $pident = sprintf("%.2f", $pident);
+                $percent_identity = 100 * abs($line[3] - $line[2] + 1) / $id_to_length{$ID};
+                $percent_identity = sprintf("%.2f", $percent_identity);
 
                 my $score = $line[4];
 
                 if ($cutoff_type == 0) {
-                    $IDs{$mydbID} = $reads2{$mydbID};
-                    $pairs{$mydbID} = join("\t", $mydbID, $hitgiID, $pident, $score, $max_evalue);
-                    if (exists $hits{$hitgiID}) {
-                        $hits{$hitgiID} += $IDs{$mydbID};
+                    $rna_id_to_cluster_size{$ID} = $id_to_cluster_size{$ID};
+                    $pairs{$ID} = join("\t", $ID, $rrna_origin, $percent_identity, $score, $max_evalue);
+                    if (exists $rrna_origin_to_cluster_size{$rrna_origin}) {
+                        $rrna_origin_to_cluster_size{$rrna_origin} += $rna_id_to_cluster_size{$ID};
                     } else {
-                        $hits{$hitgiID} = $IDs{$mydbID};
+                        $rrna_origin_to_cluster_size{$rrna_origin} = $rna_id_to_cluster_size{$ID};
                     }
                 } else {
-                    if (($max_evalue <= $cutoff1) and ($pident >= $cutoff2)) {
-                        $IDs{$mydbID} = $reads2{$mydbID};
-                        $pairs{$mydbID} = join("\t", $mydbID, $hitgiID, $pident, $score, $max_evalue);
-                        if (exists $hits{$hitgiID}) {
-                            $hits{$hitgiID} += $IDs{$mydbID};
+                    if (($max_evalue <= $evalue_cutoff) and ($percent_identity >= $identity_cutoff)) {
+                        $rna_id_to_cluster_size{$ID} = $id_to_cluster_size{$ID};
+                        $pairs{$ID} = join("\t", $ID, $rrna_origin, $percent_identity, $score, $max_evalue);
+                        if (exists $rrna_origin_to_cluster_size{$rrna_origin}) {
+                            $rrna_origin_to_cluster_size{$rrna_origin} += $rna_id_to_cluster_size{$ID};
                         } else {
-                            $hits{$hitgiID} = $IDs{$mydbID};
+                            $rrna_origin_to_cluster_size{$rrna_origin} = $rna_id_to_cluster_size{$ID};
                         }
                     }
                 }
@@ -117,28 +117,28 @@ for (my $i = 1; $i <= $II; $i++) {
     }
     close INPUT;
 
-    print "number of mapped rRNA unique reads:  ".keys( %IDs ).".\n";
+    print "number of mapped rRNA unique reads:  ".keys( %rna_id_to_cluster_size ).".\n";
     my $N_mapped = 0;
-    unlink($outfile1);
-    open(OUTPUT1, '>>', $outfile1) or die "Error opening $outfile1 : $!\n";
-    unlink($outfile2);
-    open(OUTPUT2, '>>', $outfile2) or die "Error opening $outfile2 : $!\n";
-    foreach my $key (keys %IDs) {
-        $N_mapped += $IDs{$key};
-        print OUTPUT1 "$key\t$IDs{$key}\n";
+    unlink($out_id_file);
+    open(OUTPUT1, '>>', $out_id_file) or die "Error opening $out_id_file : $!\n";
+    unlink($out_pairs_file);
+    open(OUTPUT2, '>>', $out_pairs_file) or die "Error opening $out_pairs_file : $!\n";
+    foreach my $key (keys %rna_id_to_cluster_size) {
+        $N_mapped += $rna_id_to_cluster_size{$key};
+        print OUTPUT1 "$key\t$rna_id_to_cluster_size{$key}\n";
         print OUTPUT2 "$pairs{$key}\n";
     }
     close OUTPUT1;
     close OUTPUT2;
     print "number of mapped rRNA reads:  ".$N_mapped.".\n";
 
-    print "number of mapped hits:  ".keys( %hits ).".\n\n\n";
+    print "number of mapped hits:  ".keys( %rrna_origin_to_cluster_size ).".\n\n\n";
     my $N = 0;
-    unlink($outfile3);
-    open(OUTPUT3, '>>', $outfile3) or die "Error opening $outfile3 : $!\n";
-    foreach my $key (keys %hits) {
-        $N = $N + $hits{$key};
-        print OUTPUT3 "$key\t$hits{$key}\n";
+    unlink($out_hits_id_file);
+    open(OUTPUT3, '>>', $out_hits_id_file) or die "Error opening $out_hits_id_file : $!\n";
+    foreach my $key (keys %rrna_origin_to_cluster_size) {
+        $N = $N + $rrna_origin_to_cluster_size{$key};
+        print OUTPUT3 "$key\t$rrna_origin_to_cluster_size{$key}\n";
     }
     close(OUTPUT3);
     #print "number of mapped rRNA reads:  " . $N . ".\n";
@@ -223,11 +223,11 @@ if ($II == 2) {
     close INPUT4;
     print "number of all rRAN hits:  ".keys( %hits ).".\n";
 
-    my $N = 0;
+#    my $N = 0;
     unlink($outfile3);
     open(OUTPUT3, '>>', $outfile3) or die "Error opening $outfile3 : $!\n";
     foreach my $key (keys %hits) {
-        $N = $N + $hits{$key};
+#        $N = $N + $hits{$key};
         print OUTPUT3 "$key\t$hits{$key}\n";
     }
     close(OUTPUT3);

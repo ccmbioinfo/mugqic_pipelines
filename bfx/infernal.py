cf4637ddb41d1c7e79cc65c6e8739d4e1fd4e995
Noah Reinhardt <ndrein@gmail.com>
2016-12-06 13:02:20 -0500

    Get module_infernal

--- bfx/infernal.py
+++ bfx/infernal.py
@@ -9,12 +9,9 @@ def cmscan(rfam_path, query, tblout, log_path, name=None):
     return Job(name=name,
                input_files=[rfam_path, query],
                output_files=[tblout, log_path],
-               module_entries=[['cmscan', 'infernal']],
-               command='cmscan -o {log} '
-                       '--tblout {tblout} '
-                       '{options} '
-                       '{rfam_path} '
-                       '{query}'.format(log=log_path,
+               module_entries=[['cmscan', 'module_infernal']],
+               command='cmscan -o {log} --tblout {tblout} {options} '
+                       '{rfam_path} {query}'.format(log=log_path,
                                         tblout=tblout,
                                         options=config.param('cmscan', 'options', required=False),
                                         rfam_path=rfam_path,

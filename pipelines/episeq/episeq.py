1f226bc15582562c3405f39309ae2120d1708513
Michael Li <michael.li@uwaterloo.ca>
2016-12-07 11:36:53 -0500

    Forget about && in for loops.

--- pipelines/episeq/episeq.py
+++ pipelines/episeq/episeq.py
@@ -447,7 +447,7 @@ flock -x "{table_hold}.lock" -c "echo \\"{entry}\\" >> {table_hold}" && \\
 mkdir -p {data_loc} && \\
 cp -f {output_file} {data_loc} && \\
 for i in {individual_page}; do
-    && sed -r 's%^([^0-9S][a-z ].+):(\s+.+)%|\\1|\\2|%g' $i | sed 's/\t/|/g' | awk '{script}' > "$i.md" \\
+    sed -r 's%^([^0-9S][a-z ].+):(\s+.+)%|\\1|\\2|%g' $i | sed 's/\t/|/g' | awk '{script}' > "$i.md" \\
 done
 table=$(cat {table_hold}) && \\
 pandoc \\
@@ -623,7 +623,7 @@ flock -x {table_hold}.lock -c "echo \\"{entry}\\" >> {table_hold}" && \\
 mkdir -p {data_dir} && \\
 cp -f {reports} {data_dir} && \\
 for i in {logs}; do
-    sed -r 's/:\s+/|/g' $i | egrep -v "^Option" | egrep -v "^=+$" | awk '{script}' > $i".md" && \\
+    sed -r 's/:\s+/|/g' $i | egrep -v "^Option" | egrep -v "^=+$" | awk '{script}' > $i".md" \\
 done
 table=$(cat {table_hold}) && \\
 pandoc \\

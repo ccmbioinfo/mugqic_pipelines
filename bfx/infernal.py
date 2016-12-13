b98deffa36ab0fa9c064300fbb77aa942a48f1ab
Noah Reinhardt <ndrein@gmail.com>
2016-12-13 11:39:06 -0500

    Rename rfam_path to database

--- bfx/infernal.py
+++ bfx/infernal.py
@@ -5,14 +5,14 @@ from core.config import config
 from core.job import Job
 
 
-def cmscan(rfam_path, query, tblout, log_path, name=None):
+def cmscan(database, query, tblout, log_path, name=None):
     return Job(name=name,
-               input_files=[rfam_path, query],
+               input_files=[database, query],
                output_files=[tblout, log_path],
                module_entries=[['cmscan', 'module_infernal']],
                command='cmscan -o {log} --tblout {tblout} {options} '
                        '{rfam_path} {query}'.format(log=log_path,
-                                        tblout=tblout,
-                                        options=config.param('cmscan', 'options', required=False),
-                                        rfam_path=rfam_path,
-                                        query=query))
+                                                    tblout=tblout,
+                                                    options=config.param('cmscan', 'options', required=False),
+                                                    rfam_path=database,
+                                                    query=query))

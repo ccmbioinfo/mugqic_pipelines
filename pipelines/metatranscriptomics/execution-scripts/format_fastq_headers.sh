e70339fc2911915fd0f24a4d19bb4601a90d7c88
Noah Reinhardt <ndrein@gmail.com>
2016-12-13 10:25:12 -0500

    format_fastq_headers.sh (beginning) now takes arguments for the initial FASTQs

--- pipelines/metatranscriptomics/execution-scripts/format_fastq_headers.sh
+++ pipelines/metatranscriptomics/execution-scripts/format_fastq_headers.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# $1 - path to fastq1
+# $2 - path to fastq2
 
 module load perl
 
 mkdir format_fastq_headers
 /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/main_add_subID_reads_fastq.pl \
-    ../reference-files/cow1.fastq \
+    $1 \
     format_fastq_headers/cow1_new.fastq \
-    ../reference-files/cow2.fastq \
+    $2 \
     format_fastq_headers/cow2_new.fastq

9b34d3fc16cf933869fd5f57abbba2de3e875548
Noah Reinhardt <ndrein@gmail.com>
2016-12-13 12:19:49 -0500

    Change format_fastq_headers.sh to use original fastqs (independent of ndreinhardt).  Zero configuration for execution-scripts

--- pipelines/metatranscriptomics/execution-scripts/format_fastq_headers.sh
+++ pipelines/metatranscriptomics/execution-scripts/format_fastq_headers.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# $1 - path to fastq1
-# $2 - path to fastq2
 
 module load perl
 
 mkdir format_fastq_headers
 /hpf/largeprojects/ccmbio/nreinhardt/mugqic_pipelines/pipelines/metatranscriptomics/scripts/main_add_subID_reads_fastq.pl \
-    $1 \
+    /hpf/largeprojects/ccmbio/metagenomics/Code_Test/cow1.fastq \
     format_fastq_headers/cow1_new.fastq \
-    $2 \
+    /hpf/largeprojects/ccmbio/metagenomics/Code_Test/cow2.fastq \
     format_fastq_headers/cow2_new.fastq

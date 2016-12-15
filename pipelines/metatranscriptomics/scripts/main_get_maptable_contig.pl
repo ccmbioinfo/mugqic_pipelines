7e801761aa0c61cc9f0b72368f1f2720f73ff9d6
Noah Reinhardt <ndrein@gmail.com>
2016-12-15 12:25:56 -0500

    Better variable names for main_get_maptable_contig.pl

--- pipelines/metatranscriptomics/scripts/main_get_maptable_contig.pl
+++ pipelines/metatranscriptomics/scripts/main_get_maptable_contig.pl
@@ -13,19 +13,19 @@ my $workpath = "";
 my $IDfile;
 my $IDfile2;
 my $IDfile21;
-my $infile;
-my $outfile;
-my $outfile1;
+my $contigs;
+my $out_ids;
+my $out_length;
 if ($data_type eq 'assembly') {
     my $contig_dir = "index_contigs";
     my $output_dir = "map_reads";
 
     $IDfile = $output_dir."/".$read_files."_trinity_bwa_pairs.txt";
-    $infile = $contig_dir."/".$read_files."_contigs.fasta";
+    $contigs = $contig_dir."/".$read_files."_contigs.fasta";
 
-    $outfile = $output_dir."/".$read_files."_contigs_IDs.txt";
-    $outfile1 = $output_dir."/".$read_files."_contigs_IDs_length.txt";
-    print "Outputfile: $outfile1\n\n";
+    $out_ids = $output_dir."/".$read_files."_contigs_IDs.txt";
+    $out_length = $output_dir."/".$read_files."_contigs_IDs_length.txt";
+    print "Outputfile: $out_length\n\n";
 } elsif ($data_type eq 'bwa') {
     $IDfile = $workpath.$read_files."_contigs_IDs.txt";
     $IDfile2 = $workpath.$read_files."_contigs_micro_cds_bwa_IDs.txt";
@@ -41,73 +41,74 @@ if ($data_type eq 'assembly') {
 }
 
 if ($data_type eq 'assembly') {
-    my %IDs;
+    my %id_to_num_reads;
     open(INPUT0, $IDfile) or die "Error opening $IDfile : $!\n";
     while  ( my $line = <INPUT0>) {
         chomp($line);
         my @line1 = split(/\t/, $line);
-        if (exists $IDs{$line1[1]}) {
-            $IDs{$line1[1]}++;
+        if (exists $id_to_num_reads{$line1[1]}) {
+            $id_to_num_reads{$line1[1]}++;
         } else {
-            $IDs{$line1[1]} = 1;
+            $id_to_num_reads{$line1[1]} = 1;
         }
     }
     close INPUT0;
 
-    my %contigs;
+    my %id_to_length;
     my $total = 0;
-    unlink($outfile);
-    open(OUTPUT, '>>', $outfile) or die "Error opening $outfile : $!\n";
-    unlink($outfile1);
-    open(OUTPUT1, '>>', $outfile1) or die "Error opening $outfile1 : $!\n";
-    my $seqIO = Bio::SeqIO->new( -file => $infile, -format => 'fasta' );
+    unlink($out_ids);
+    open(OUTPUT, '>>', $out_ids) or die "Error opening $out_ids : $!\n";
+    unlink($out_length);
+    open(OUTPUT1, '>>', $out_length) or die "Error opening $out_length : $!\n";
+    my $seqIO = Bio::SeqIO->new( -file => $contigs, -format => 'fasta' );
     while (my $seqobj = $seqIO->next_seq) {
-        my $head = $seqobj->display_id;
-        $contigs{$head} = $seqobj->length;
+        my $id = $seqobj->display_id;
+        $id_to_length{$id} = $seqobj->length;
 
-        if (not exists $IDs{$head}) {
-            $IDs{$head} = 1;
+        if (not exists $id_to_num_reads{$id}) {
+            $id_to_num_reads{$id} = 1;
         }
-        $total += $IDs{$head};
-        print OUTPUT "$head\t$IDs{$head}\n";
-        print OUTPUT1 "$head\t$IDs{$head}\t$contigs{$head}\n";
+        $total += $id_to_num_reads{$id};
+        print OUTPUT "$id\t$id_to_num_reads{$id}\n";
+        print OUTPUT1 "$id\t$id_to_num_reads{$id}\t$id_to_length{$id}\n";
     }
     close OUTPUT1;
     close OUTPUT;
-    print "number of all contigs:  ".keys( %contigs ).".\n";
+    print "number of all contigs:  ".keys( %id_to_length ).".\n";
     print "total number of reads mapping to contigs = $total.\n\n\n";
-} else {
-    my %IDs;
-    open(INPUT0, $IDfile) or die "Error opening $IDfile : $!\n";
-    while  ( my $line = <INPUT0>) {
-        chomp($line);
-        my @line1 = split(/\t/, $line);
-        $IDs{$line1[0]} = $line1[1];
-    }
-    close INPUT0;
-
-    my $total = 0;
-    open(INPUT2, $IDfile2) or die "Error opening $IDfile2 : $!\n";
-    while  ( my $line = <INPUT2>) {
-        chomp($line);
-        if (exists $IDs{$line}) {
-            $total += $IDs{$line};
-        } else {
-            $total += 1;
-        }
-    }
-    close INPUT2;
-    open(INPUT2, $IDfile21) or die "Error opening $IDfile21 : $!\n";
-    while  ( my $line = <INPUT2>) {
-        chomp($line);
-        if (exists $IDs{$line}) {
-            $total += $IDs{$line};
-        } else {
-            $total += 1;
-        }
-    }
-    close INPUT2;
-    print "Total number of mapped-reads = $total.\n\n\n";
 }
+#} else {
+#    my %IDs;
+#    open(INPUT0, $IDfile) or die "Error opening $IDfile : $!\n";
+#    while  ( my $line = <INPUT0>) {
+#        chomp($line);
+#        my @line1 = split(/\t/, $line);
+#        $IDs{$line1[0]} = $line1[1];
+#    }
+#    close INPUT0;
+#
+#    my $total = 0;
+#    open(INPUT2, $IDfile2) or die "Error opening $IDfile2 : $!\n";
+#    while  ( my $line = <INPUT2>) {
+#        chomp($line);
+#        if (exists $IDs{$line}) {
+#            $total += $IDs{$line};
+#        } else {
+#            $total += 1;
+#        }
+#    }
+#    close INPUT2;
+#    open(INPUT2, $IDfile21) or die "Error opening $IDfile21 : $!\n";
+#    while  ( my $line = <INPUT2>) {
+#        chomp($line);
+#        if (exists $IDs{$line}) {
+#            $total += $IDs{$line};
+#        } else {
+#            $total += 1;
+#        }
+#    }
+#    close INPUT2;
+#    print "Total number of mapped-reads = $total.\n\n\n";
+#}
 
 exit 0;
